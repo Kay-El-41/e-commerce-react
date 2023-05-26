fd13dccd0e976fc051d9ec65e09a5f6ed40b0e5b
Kay-El-41 <z.khantolino@gmail.com>
2023-05-26 15:47:43 +0630

    margin

--- src/components/landingPage/HeroSection.jsx
+++ src/components/landingPage/HeroSection.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const HeroSection = () => {
-
   return (
     <section
       id="hero"
@@ -16,7 +15,7 @@ const HeroSection = () => {
         nesciunt.
       </p>
       <Link to="/products/page/1">
-        <button className="bg-white bg- px-5 py-1 rounded-2xl text-orange-500 hover:scale-105 ease transition-all duration-300 font-semibold shadow tracking-wide">
+        <button className="mt-3 bg-white px-5 py-1 rounded-2xl text-orange-500 hover:scale-105 ease transition-all duration-300 font-semibold shadow tracking-wide">
           Browse Now
         </button>
       </Link>
